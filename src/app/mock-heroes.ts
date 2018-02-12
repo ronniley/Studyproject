80841582470a6a749d79e4752d28cd20f7ff5ce4
Weronika P <ronnileypl@gmail.com>
2018-02-12 15:32:29 +0100

    Pierwszy upload projektu na gita

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+﻿import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Kacper' },
+    { id: 12, name: 'Janek' },
+    { id: 13, name: 'Maks' },
+    { id: 14, name: 'Maniek' },
+    { id: 15, name: 'Klemens' },
+    { id: 16, name: 'Olaf' },
+    { id: 17, name: 'Iwo' },
+    { id: 18, name: 'Bartek' },
+    { id: 19, name: 'Tomek' },
+    { id: 20, name: 'Sławek' }
+];
